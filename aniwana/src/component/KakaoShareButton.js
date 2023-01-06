0ba60cd7e610c7c8d80424317a09504e074e2951
Kanon <sunny100487@naver.com>
2023-01-06 22:01:50 +0900

    Update KakaoShareButton.js

--- aniwana/src/component/KakaoShareButton.js
+++ aniwana/src/component/KakaoShareButton.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 const { Kakao } = window;
 
 const KakaoShareButton = ({ data }) => {
-  const url = "https://catmbti10.netlify.app/";
+  const url = "https://catmbti10.netlify.app/"; // url은 다른 테스트의 링크입니다 아직 배포전이어서 이후에 채워넣을 생각입니다.
   const resultUrl = window.location.href;
 
   React.useEffect(() => {
@@ -18,8 +18,8 @@ const KakaoShareButton = ({ data }) => {
     Kakao.Link.sendDefault({
       objectType: 'feed',
       content: {
-        title: '예비집사 판별기 결과',
-        description: `예비 집사님이 고양이를 키운다면 가장 잘맞는 고양이는 ${data.name}입니다.`,
+        title: '애니메이션 추천 테스트 결과',
+        description: `가장 잘맞는 애니메이션은 ${data.name}입니다.`,
         imageUrl: url + data.image,
         link: {
           mobileWebUrl: resultUrl,
