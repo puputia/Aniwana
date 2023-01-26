5331c66762a60c4966c358a1ba065db5eb08a8d7
Kanon <sunny100487@naver.com>
2023-01-26 20:54:33 +0900

    version up

--- aniwana/src/App.js
+++ aniwana/src/App.js
@@ -2,7 +2,8 @@
 import Home from './pages/Home';
 import Question from './pages/Question';
 import Result from './pages/Result';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom'
+
 import './App.css';
 
 function App() {
